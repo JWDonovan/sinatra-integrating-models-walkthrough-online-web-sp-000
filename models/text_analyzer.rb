152062761a2322b2a003cb678e250cbe99d870e2
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-19 19:55:28 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,21 +1,24 @@
+
 # Your TextAnalyzer model code will go here.
+
 class TextAnalyzer
   attr_reader :text
 
   def initialize(text)
-    @text = text
+    @text = text.downcase
   end
 
-  def number_of_words
-    @text.split(" ").count
+  def count_of_words
+    words = text.split(" ")
+    words.count
   end
 
-  def number_of_vowels
-    @text.scan(/[aeiou]/i).count
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
   end
 
-  def number_of_consonants
-    @text.scan(/[bcdfghjklmnpqrstvwxyz]/i).count
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
 
   def most_used_letter
