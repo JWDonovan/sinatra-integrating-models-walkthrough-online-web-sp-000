2287563dfcf59b87f1a94dc8646ff2a1d7fda927
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-19 19:49:57 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,6 +1,12 @@
 # Your TextAnalyzer model code will go here.
 class TextAnalyzer
+  attr_reader :text
+
   def initialize(text)
     @text = text
+  end
+
+  def number_of_words
+    @text.split(" ").count
   end
 end
